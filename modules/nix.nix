368ec0df747b6dc1b4fde5864ed43b280eed1ec1
Poly2it <84731064+Poly2it@users.noreply.github.com>
2024-10-06 22:03:45 +0200

    Enable Nix cache for CUDA packages

--- modules/nix.nix
+++ modules/nix.nix
@@ -9,6 +9,15 @@
     options = "--delete-older-than 5d";
   };
 
+  nix.settings = {
+    substituters = [
+      "https://cuda-maintainers.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    ];
+  };
+
   programs.nix-ld.enable = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
